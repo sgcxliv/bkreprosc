b33a4cab998bea9dd393055cbc7146ab1a4372dc
realhautegirl <115960944+sgcxliv@users.noreply.github.com>
2025-05-13 03:02:31 -0700

    Update mergesurp.py

--- mergesurp.py
+++ mergesurp.py
@@ -1,5 +1,5 @@
 # to merge cleaned cloze data (no mismatched items) with spr data
-# any time i need to merge anything i just use a versin of this 
+# any time i need to merge 2 csvs or anything i just use a versin of this 
 import os
 import pandas as pd
 
